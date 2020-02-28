3dfff28e629be217d1b6e79a735d7eda370eb4d4
Ruben Gomes <rbnpontes@gmail.com>
2020-02-28 13:27:22 -0300

    Created Order method and implemented Interface at service

--- Business/GovernmentExpenses.Expenses/Services/ExpenseService.cs
+++ Business/GovernmentExpenses.Expenses/Services/ExpenseService.cs
@@ -1,17 +1,48 @@
-﻿using GovernmentExpenses.Expenses.Entities;
+﻿using GovernmentExpenses.Core;
+using GovernmentExpenses.Expenses.Entities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace GovernmentExpenses.Expenses.Services
 {
     public interface IExpenseService
     {
-        ExpenseDTO FetchTotalExpenses();
-        IDictionary<string, ExpenseDTO> FetchTotalExpensesByProp();
-        IDictionary<string, ExpenseDTO> FetchGroupItemsPerMonth();
-        
+        IExpenseResult FetchTotalExpenses();
+        IDictionary<string, IExpenseResult> FetchTotalExpensesByProp(string prop);
+        IDictionary<string, IReadOnlyList<ExpenseDTO>> FetchExpensesGroupByProp(string prop, int page = 0, int pageSize = 10, string orderBy = "", bool orderDesc = false);
+        IReadOnlyList<ExpenseDTO> FetchAll(int page = 0, int pageSize = 10, string orderBy = "id", bool orderDesc = false);
+        IReadOnlyList<ExpensePair> FetchEnum(string prop);
     }
-    internal class ExpenseService : IExpenseService
+    internal partial class ExpenseService : IExpenseService
     {
+        private IEnumerable<Expense> TryOrderList(IEnumerable<Expense> list, string orderBy, bool desc){
+            if (string.IsNullOrEmpty(orderBy) || !OrderKeyPairs.ContainsKey(orderBy))
+                return list;
+            if (desc)
+                return list.OrderByDescending(OrderKeyPairs[orderBy]);
+            return list.OrderBy(OrderKeyPairs[orderBy]);
+        }
+        public IReadOnlyList<ExpenseDTO> FetchAll(int page = 0, int pageSize = 10, string orderBy = "id", bool orderDesc = false)
+        {
+            var values = TryOrderList(Repository.All().Page(page, pageSize), orderBy, orderDesc);
+            return values.Select(x => new ExpenseDTO(x)).ToList().AsReadOnly();
+        }
+        public IReadOnlyList<ExpensePair> FetchEnum(string prop)
+        {
+            throw new NotImplementedException();
+        }
+        public IDictionary<string, IReadOnlyList<ExpenseDTO>> FetchExpensesGroupByProp(string prop, int page = 0, int pageSize = 10, string orderBy = "", bool orderDesc = false)
+        {
+            throw new NotImplementedException();
+        }
+        public IExpenseResult FetchTotalExpenses()
+        {
+            throw new NotImplementedException();
+        }
+        public IDictionary<string, IExpenseResult> FetchTotalExpensesByProp(string prop)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Business/GovernmentExpenses.Expenses/Services/ExpenseServiceConfigs.cs
@@ -0,0 +1,52 @@
+﻿using GovernmentExpenses.Core;
+using GovernmentExpenses.Expenses.Entities;
+using GovernmentExpenses.Expenses.Repository;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GovernmentExpenses.Expenses.Services
+{
+    internal partial class ExpenseService
+    {
+        public ILogger Logger { get; private set; }
+        private IRepository<Expense> repository_;
+        public IRepository<Expense> Repository
+        {
+            get
+            {
+                if (repository_ == null)
+                    repository_ = new ExpenseRepository(Logger);
+                return repository_;
+            }
+        }
+        private Dictionary<string, Func<Expense, object>> OrderKeyPairs = new Dictionary<string, Func<Expense, object>>
+        {
+            {"id", (x) => x.Id},
+            {"ano_movimentacao", (x) => x.AnoMovimentacao},
+            {"mes_movimentacao", (x)=> x.MesMovimentacao},
+            {"orgao", (x) => x.Orgao.Code},
+            {"unidade", (x)=> x.Unidade.Code},
+            {"categoria",(x)=> x.CategoriaEconomica.Code},
+            {"grupo", (x)=> x.GrupoDespesa.Code},
+            {"mod_aplicacao",(x)=> x.ModalidadeAplicacao.Code},
+            {"elemento", (x)=> x.Elemento.Code},
+            {"sub_elemento", (x)=> x.SubElemento.Code},
+            {"funcao", (x)=> x.Funcao.Code },
+            {"sub_funcao", (x)=> x.SubFuncao.Code },
+            {"programa", (x)=> x.Programa.Code},
+            {"acao", (x)=>x.Acao.Code },
+            {"fonte_recurso", (x)=> x.FonteRecurso.Code},
+            {"empenho_ano", (x) => x.EmpenhoAno},
+            {"empenho_mod", (x)=> x.EmpenhoModalidade.Code },
+            {"empenho_num", (x)=> x.EmpenhoNumero},
+            {"sub_empenho", (x)=> x.SubEmpenho},
+            {"ind_sub_empenho", (x)=> x.IndicadorSubEmpenho },
+            {"credor", (x) => x.Credor.Code},
+            {"mod_licitacao", (x)=> x.ModalidadeLicitacao.Code },
+            {"valor_empenhado", (x) => x.ValorEmpenhado},
+            {"valor_pago", (x)=> x.ValorPago}
+        };
+    }
+}
